7a72df251295e7110e177af5fb89000576e18e57
Brian Slaby <brian.slaby@gmail.com>
2023-12-13 12:29:31 -0500

    started building desktop layout, currently things are messy

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,54 @@ import Nav from './components/nav/Nav'
 import Button from './components/Button'
 import Banner from './components/Banner'
 
+const WindowSizeContext = React.createContext()
+
 export default function App() {
 const [navVisible, setNavVisible] = React.useState(false)
+const [windowWidth, setWindowWidth] = React.useState(0)
 
 const overlayVisibility = navVisible ? "overlay-active" : ""
+const layoutSwitchWidth = 1000
 
 function toggleNav() {
   setNavVisible(prevState => !prevState)
 }
 
+function updateWindowSize() {
+  const width = window.innerWidth
+  setWindowWidth(width)
+}
+
+React.useEffect(() => {
+  updateWindowSize()
+  window.addEventListener("resize", updateWindowSize)
+  return () => window.removeEventListener("resize", updateWindowSize)
+}, [])
+
+console.log(windowWidth)
 
   return (
-    <>
+    <WindowSizeContext.Provider value={{windowWidth, layoutSwitchWidth}}>
+      {windowWidth < layoutSwitchWidth && 
+      <>
       <div className={`nav-container-mobile ${navVisible ? "" : "hidden"}`}>
-        <Nav toggleNav={toggleNav}/>
+        <Nav 
+          toggleNav={toggleNav}/>
       </div>
       <div className={`bg-overlay ${overlayVisibility}`}></div>
+      </>}
+      
       
       <header>
         <img src="./public/images/logo.svg" />
 
+        {windowWidth < layoutSwitchWidth ?
         <Button classProp="open-nav-btn" clickProp={toggleNav}>
           <img src="./public/icons/icon-menu.svg"/>
-        </Button>
+        </Button> :
+        <Nav/>}
+
+        
       </header>
       <main>
         <img 
@@ -53,6 +78,8 @@ function toggleNav() {
         </section>
       </main>
       
-    </>
+    </WindowSizeContext.Provider>
   )
 }
+
+export { WindowSizeContext }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,10 +3,13 @@ import Button from "../Button"
 import Dropdown from "./Dropdown"
 import NavItem from "./NavItem"
 import DropdownButton from "./DropdownButton"
+import { WindowSizeContext } from "../../App"
 
 export default function Nav({ toggleNav }) {
     const [featuresOpen, setFeaturesOpen] = React.useState(false)
     const [companyOpen, setCompanyOpen] = React.useState(false)
+
+    const { windowWidth, layoutSwitchWidth } = React.useContext(WindowSizeContext)
 
     function toggleFeaturesOpen() {
         setFeaturesOpen(prevState => !prevState)
@@ -16,13 +19,33 @@ export default function Nav({ toggleNav }) {
         setCompanyOpen(prevState => !prevState)
     }
 
+    // NOTE: get rid of the inline styling.
+        // conditionally add mobile and desktop classes and style in CSS file
+        // think about secondary nav dropdowns
+            // keep as is in mobile
+            // they're essentially modals in desktop
+            // the NavItem is the parent, relative positioning?
+
+    const flexDirection = windowWidth < layoutSwitchWidth ? "column" : "row"
+
     return(
-        <nav className={`main-nav`}>
+        <nav 
+        className={`main-nav`}
+        style={{flexDirection: flexDirection}}>
+            
+            {windowWidth < layoutSwitchWidth && 
             <Button classProp="close-nav-btn" clickProp={toggleNav}>
                 <img src="./public/icons/icon-close-menu.svg"/>
-            </Button>
+            </Button>}
 
-            <ul className="nav-links nav-links-primary">
+            <ul 
+            className="nav-links nav-links-primary"
+            style={{ 
+                display: "flex", 
+                flexDirection: flexDirection,
+                gap: "1em",
+                alignItems: "flex-start",
+                justifyContent: "center" }}>
                 <NavItem>Features 
                 <DropdownButton 
                     open={featuresOpen} 
